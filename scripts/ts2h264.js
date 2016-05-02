783ed7c191550b300ca6d586aa808aaaec80e7ff
flny__ <nkl.flny@gmail.com>
2016-05-02 17:10:32 +0000

    work

--- scripts/ts2h264.js
+++ scripts/ts2h264.js
@@ -9,21 +9,36 @@ var tsDir = process.argv[2],
     mp4Dir = process.argv[3]
 ;
 
+var encode, onSuccess, onError
+;
 
-var encode = function(srcPath, dstPath) {
+
+
+encode = function(srcPath, dstPath) {
     ffmpeg()
       .input(srcPath)
       .audioCodec('libmp3lame')
       .audioBitrate(192)
       .videoCodec('libx264')
       .addOptions(['-crf 20'])
+      .on('end', onSuccess)
+      .on('error', onError)
+      .saveToFile(dstPath);
+};
+
+onSuccess = function(stdout, stderr) {
+    console.log(stdout);
+};
+
+onError = function(err) {
+    console.log(err.message);
 };
 
 
 fs.readdir(tsDir, function(err, files) {
     if(err) throw err;
     files.filter(function(file) {
-        if(fs.statSync(tsDir + file).isDirectory()) {
+        if(fs.statSync(tsDir + file).isFile()) {
             console.log(tsDir + file);
         }
     })
