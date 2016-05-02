9bc4df3ef5e3a46c4f2925dc056f7a004b1c2b54
flny__ <nkl.flny@gmail.com>
2016-05-02 16:52:56 +0000

    work

--- scripts/ts2h264.js
+++ scripts/ts2h264.js
@@ -1,17 +1,30 @@
 #!/usr/bin/env node
 
-var ffmpeg = require("fluent-ffmpeg");
-var fs = require("fs-extra");
+var ffmpeg = require("fluent-ffmpeg"),
+    fs = require("fs-extra")
+;
 
-var tsRoot = "/mnt/pub/movie/tv/raw/";
+// var tsRoot = "/mnt/pub/movie/tv/raw/";
+var tsDir = process.argv[2],
+    mp4Dir = process.argv[3]
+;
 
 
+var encode = function(srcPath, dstPath) {
+    ffmpeg()
+      .input(srcPath)
+      .audioCodec('libmp3lame')
+      .audioBitrate(192)
+      .videoCodec('libx264')
+      .addOptions(['-crf 20'])
+};
 
-fs.readdir(tsRoot, function(err, files) {
+
+fs.readdir(tsDir, function(err, files) {
     if(err) throw err;
     files.filter(function(file) {
-        if(fs.statSync(tsRoot + file).isDirectory()) {
-            console.log(tsRoot + file);
+        if(fs.statSync(tsDir + file).isDirectory()) {
+            console.log(tsDir + file);
         }
     })
 })
