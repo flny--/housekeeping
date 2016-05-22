fcfae98d48da92a60189342f4c1866b9bba669c3
flny__ <nkl.flny@gmail.com>
2016-05-22 06:12:43 +0000

    work

---
+++ scripts/sendDdnsRec.js
@@ -0,0 +1,40 @@
+#!/usr/bin/env node
+
+var ExtIP = require('external-ip'),
+    Route53 = require('nice-route53')
+;
+
+var configMap = {
+        zoneId  : process.env.ROUTE53_ZONE_ID,
+        iam_key : process.env.IAM_DDNS_KEY,
+        iam_sec : process.env.IAM_DDNS_SECRET,
+    },
+    getIP = new ExtIP(),
+    r53 = new Route53(configMap.iam_key, configMap.iam_sec),
+    onReceiveIp, sendRecord
+;
+
+
+sendRecord = function(ip) {
+    r53.records(configMap.zoneId, function(err, records) {
+        if(err) throw err;
+        
+        var aRec = records.filter(function(record, index, array){
+            console.log(record);
+            return (record.name == 'flny.ch.' && record.type == 'A');
+        })[0];
+    })
+}
+
+
+onReceiveIp = function(err, ip) {
+    if(err) {
+        throw err;
+    }
+    console.log(ip);
+    sendRecord(ip)
+}
+
+
+getIP(onReceiveIp);
+
