fb29fe628538e8fc05ac9e05a743b968db3de6b5
flny__ <nkl.flny@gmail.com>
2016-06-06 10:12:13 +0000

    work

--- scripts/scanPort.js
+++ scripts/scanPort.js
@@ -1,7 +1,7 @@
 #!/usr/bin/env node
 
 var scanner = require('node-netcat').portscan(),
-    fs = require('fs')
+    logger = require('./logger')
 ;
 
 
@@ -10,15 +10,27 @@ var configMap = {
   ports : ['43021', '53', '445', '8200', '8201'],
   udpErrorStr : 'protocol \"udp\" with message',
   msgPath : '/mnt/pub/misc/monitor/'
+},
+errMessages,
+scanPorts
+;
+
+
+scanPorts = function() {
+  var messages = '';
+  configMap.ports.forEach(function(port) {
+    scanner.run(configMap.host, port, function(err, res) {
+      if(err && err.indexOf(configMap.udpErrorStr) == -1) {
+        messages += err + '\n';
+      }
+    });
+  });
+  return messages;
 }
 
 
-var msgFile = configMap.msgPath + "system_port_" + (new Date()).getTime();
+errMessages = scanPorts();
+if(errMessages == '') {
+  logger.log(logger.categorySystemError, errMessages);
+}
 
-configMap.ports.forEach(function(port) {
-  scanner.run(configMap.host, port, function(err, res) {
-    if(err && err.indexOf(configMap.udpErrorStr) == -1) {
-      fs.appendFile(msgFile, err + "\n");
-    }
-  });
-});
