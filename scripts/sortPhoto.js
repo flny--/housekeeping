3c0d30b3eea7aab6d654d50865e6c75973a7ed5a
flny__ <nkl.flny@gmail.com>
2016-06-13 13:30:41 +0000

    work

--- scripts/sortPhoto.js
+++ scripts/sortPhoto.js
@@ -1,7 +1,36 @@
 #!/usr/bin/env node
 
+var readdir = require("recursive-readdir"),
+    path    = require("path"),
+    fs      = require("fs-extra"),
+    exif    = require("exif")
+;
 
 var configMap = {
-        poolPath : "/mnt/pub/image/photo/pool/"
+        poolPath   : "/mnt/pub/image/photo/pool/",
+        targetPath : "/mnt/pub/image/photo/"
 
-    }
+    },
+    filterFile, sortOneFile
+;
+
+
+filterFile = function(file, stats) {
+    return stats.isDirectory() || 
+        !path.basename(file).toLowerCase().endsWith(".jpg");
+};
+
+
+sortOneFile = function(file) {
+    exif(file, function(err, exifObj){
+        if(err) throw err;
+        console.log(exifObj["date time original"]);
+    });
+};
+
+
+
+readdir(configMap.poolPath, [filterFile], function(err, files) {
+    if(err) throw err;
+    files.forEach(sortOneFile);
+});
